3f1533c6fa78f1675eb3ae5be3d4400113fbd5cf
doctor_ew <doctor_ew@protonmail.com>
2023-12-22 00:33:12 +0000

    troubleshooting pvc creation on kube-plex

--- projects/nfs/install.sh
+++ projects/nfs/install.sh
@@ -2,32 +2,4 @@
 
 set -e
 
-# TODO DELETE FOR TESTING ONLY
-kubectl delete -f projects/nfs/blackbox.yaml || true
-helm uninstall csi-driver-nfs -n kube-system || true
-
-# setup nfs server INSTALL ON ALL NODES
-sudo apt install -y nfs-kernel-server nfs-common || true
-sudo mkdir -p /mnt/k8s_volumes
-sudo chown nobody:nogroup /mnt/k8s_volumes
-sudo cp -f /etc/exports.bak /etc/exports
-echo "/mnt/k8s_volumes *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
-sudo exportfs -a
-
-
-# helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-# helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-#     --set nfs.server=192.168.1.214 \
-#     --set nfs.path=/mnt/k8s_volumes
-
-# helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
-# helm repo update
-# helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
-#     --namespace kube-system \
-#     --set kubeletDir=/var/snap/microk8s/common/var/lib/kubelet \
-#     --set externalSnapshotter.enabled=true \
-#     --set controller.runOnControlPlane=true \
-#     --set controller.replicas=2 \
-#     --set controller.dnsPolicy=ClusterFirstWithHostNet \
-#     --set node.dnsPolicy=ClusterFirstWithHostNet \
-#     --set kubeletDir="/var/snap/microk8s/common/var/lib/kubelet"
+kubectl patch storageclass nfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
--- projects/plex/install.sh
+++ projects/plex/install.sh
@@ -2,37 +2,17 @@
 
 set -e
 
-# TODO DELETE FOR TESTING ONLY
-kubectl delete -f projects/plex/blackbox.yaml || true
-# helm uninstall csi-driver-nfs -n kube-system || true
+# kubectl apply -f projects/plex/blackbox.yaml
+# kubectl logs --selector app=csi-nfs-controller -n kube-system -c nfs
 
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-    --set nfs.server=192.168.1.214 \
-    --set nfs.path=/mnt/blackbox
-
-# helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
-# helm repo update
-# helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
-#     --namespace kube-system \
-#     --set kubeletDir=/var/snap/microk8s/common/var/lib/kubelet \
-#     --set externalSnapshotter.enabled=true \
-#     --set controller.runOnControlPlane=true \
-#     --set controller.replicas=2 \
-#     --set controller.dnsPolicy=ClusterFirstWithHostNet \
-#     --set node.dnsPolicy=ClusterFirstWithHostNet \
-#     --set kubeletDir="/var/snap/microk8s/common/var/lib/kubelet"
-
-kubectl apply -f projects/plex/blackbox.yaml
-kubectl logs --selector app=csi-nfs-controller -n kube-system -c nfs
-
-# git clone git@github.com:munnerz/kube-plex.git projects/plex/kube-plex || true
-# source projects/plex/.env
-# kubectl create namespace plex || true
-# helm install plex ./projects/plex/kube-plex/charts/kube-plex \
-#     --namespace plex \
-#     --set claimToken=$PLEX_CLAIM \
-#     --set service.type=NodePort \
-#     --set service.port=32400 \
-#     --set persistence.data.claimName=blackbox-nfs-pvc
-
+git clone git@github.com:munnerz/kube-plex.git projects/plex/kube-plex || true
+source projects/plex/.env
+kubectl create namespace plex || true
+helm install plex ./projects/plex/kube-plex/charts/kube-plex \
+    --namespace plex \
+    --set claimToken=$PLEX_CLAIM \
+    --set service.type=NodePort \
+    --set service.nodePort=32500 \
+    --set 
+    # --set ingress.enabled=true \
+    # --set ingress.hosts[0]=kube-plex.doctor-ew.com 
