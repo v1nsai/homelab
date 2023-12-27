70def14c3ac938c60f959b9109a367a8b3fdf288
doctor_ew <doctor_ew@protonmail.com>
2023-12-27 16:32:59 +0000

    kube-plex troubleshooting

--- projects/plex/install.sh
+++ projects/plex/install.sh
@@ -5,14 +5,22 @@ set -e
 # kubectl apply -f projects/plex/blackbox.yaml
 # kubectl logs --selector app=csi-nfs-controller -n kube-system -c nfs
 
+helm uninstall -n plex plex || true
+
 git clone git@github.com:munnerz/kube-plex.git projects/plex/kube-plex || true
 source projects/plex/.env
 kubectl create namespace plex || true
 helm install plex ./projects/plex/kube-plex/charts/kube-plex \
     --namespace plex \
     --set claimToken=$PLEX_CLAIM \
-    --set service.type=NodePort \
-    --set service.nodePort=32500 \
-    --set 
-    # --set ingress.enabled=true \
-    # --set ingress.hosts[0]=kube-plex.doctor-ew.com 
+    --set timezone=America/New_York 
+    # --set service.type=NodePort \
+    # --set service.nodePort=32400
+    # --set service.externalTrafficPolicy=Local
+    # --set persistence.transcode.storageClass=nfs \
+    # --set persistence.data.storageClass=nfs \
+    # --set persistence.config.storageClass=nfs 
+    # --set ingress.enabled=false \
+    # --set ingress.hosts[0]=plex.doctor-ew.com \
+    # --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
+    # --set ingress.annotations."kubernetes\.io/tls-acme"=false \
