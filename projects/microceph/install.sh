60de6a0a95ca61ad9beee47ca3e0a99d07dac5ec
doctor_ew <doctor_ew@protonmail.com>
2024-01-02 19:01:24 +0000

    ceph fully running and tested

--- projects/microceph/install.sh
+++ projects/microceph/install.sh
@@ -4,46 +4,47 @@ set -e
 # source projects/microk8s/.env
 
 # Windows
-# Run this after setting up master node.  Don't use windows node for master.
-# winget install multipass --classic
-# multipass delete --purge $WINDOWS_NODE_NAME
-# WINDOWS_NODE_NAME=""
-# multipass launch  --name ${WINDOWS_NODE_NAME}  --cpus 4  --memory 16G  --disk 20G  --network bridged --mount C:/Users/doctor_ew/multipass/${WINDOWS_NODE_NAME}:/mnt/${WINDOWS_NODE_NAME}
-# multipass shell ${WINDOWS_NODE_NAME}
-# sudo snap install microceph
-# sudo microceph init
+## Run this after setting up master node.  Don't use windows node for master.  Just....don't.
+winget install multipass --classic
+multipass delete --purge $WINDOWS_NODE_NAME
+WINDOWS_NODE_NAME=""
+multipass launch  --name ${WINDOWS_NODE_NAME}  --cpus 4  --memory 16G  --disk 20G  --network bridged --mount C:/Users/doctor_ew/multipass/${WINDOWS_NODE_NAME}:/mnt/${WINDOWS_NODE_NAME}
+multipass shell ${WINDOWS_NODE_NAME}
+sudo snap install microceph
+sudo microceph init
 # set the interface to use the bridged one, everything else default except no to adding disks
 # Follow linux instructions next
 
 # Linux
-# echo "Setting up microceph..."
-# sudo snap install microceph
-# sudo snap refresh --hold microceph # hold updates
-# sudo microceph cluster bootstrap
+## Use for master node
+echo "Setting up microceph..."
+sudo snap install microceph
+sudo snap refresh --hold microceph # hold updates
+sudo microceph cluster bootstrap
 
-# echo "Creating and adding virtual disks to microceph cluster..."
-# CEPH_DATA_DIR="/mnt/ceph"
-# sudo mkdir -p $CEPH_DATA_DIR
-# for i in {0..4}; do
-#     echo "Creating virtual disk $CEPH_DATA_DIR/loop$i..."
-#     sudo dd if=/dev/zero of=$CEPH_DATA_DIR/loop$i bs=1M count=10240
-#     CEPH_LOOP_DISK=$(sudo losetup -f)
-#     sudo losetup $CEPH_LOOP_DISK $CEPH_DATA_DIR/loop$i
-#     echo "Adding virtual disk $CEPH_DATA_DIR/loop$i to ceph..."
-#     sudo microceph disk add --wipe $CEPH_LOOP_DISK
-# done
+echo "Creating and adding virtual disks to microceph cluster..."
+CEPH_DATA_DIR="/mnt/ceph"
+sudo mkdir -p $CEPH_DATA_DIR
+for i in {0..4}; do
+    echo "Creating virtual disk $CEPH_DATA_DIR/loop$i..."
+    sudo dd if=/dev/zero of=$CEPH_DATA_DIR/loop$i bs=1M count=10240
+    CEPH_LOOP_DISK=$(sudo losetup -f)
+    sudo losetup $CEPH_LOOP_DISK $CEPH_DATA_DIR/loop$i
+    echo "Adding virtual disk $CEPH_DATA_DIR/loop$i to ceph..."
+    sudo microceph disk add --wipe $CEPH_LOOP_DISK
+done
 
 # Add external disks here
 # sudo microceph disk add --wipe /dev/sdc
 
-echo "Re-adding disks to cluster..."
-CEPH_DATA_DIR="/mnt/ceph"
-CEPH_LOOP_DISKS=$(sudo losetup -a | grep "$CEPH_DATA_DIR" | awk '{print $1}' | tr -d ":")
-echo $CEPH_LOOP_DISKS
-for CEPH_LOOP_DISK in $CEPH_LOOP_DISKS; do
-    echo "Adding virtual disk $CEPH_LOOP_DISK to ceph..."
-    sudo microceph disk add --wipe $CEPH_LOOP_DISK
-done
+# Too lazy to figure out why microceph times out when trying to remove nodes that are down
+# CEPH_DATA_DIR="/mnt/ceph"
+# CEPH_LOOP_DISKS=$(sudo losetup -a | grep "$CEPH_DATA_DIR" | awk '{print $1}' | tr -d ":")
+# echo $CEPH_LOOP_DISKS
+# for CEPH_LOOP_DISK in $CEPH_LOOP_DISKS; do
+#     echo "Adding virtual disk $CEPH_LOOP_DISK to ceph..."
+#     sudo microceph disk add --wipe $CEPH_LOOP_DISK
+# done
 
 # echo "Adding nodes..."
 # sudo microceph cluster add bigrig
--- projects/microceph/rook-ceph/install.sh
+++ projects/microceph/rook-ceph/install.sh
@@ -2,23 +2,33 @@
 
 set -e
 
-# echo "Setting up microk8s ceph addon..."
-# microk8s enable rook-ceph
-# sudo microk8s connect-external-ceph \
+echo "Setting up microk8s ceph addon..."
+microk8s enable rook-ceph
+sudo microk8s connect-external-ceph
 #     --ceph-conf /var/snap/microceph/current/conf/ceph.conf \
 #     --keyring /var/snap/microceph/current/conf/ceph.keyring \
 #     --rbd-pool-auto-create \
 #     --rbd-pool microk8s-rbd
 
-echo "Setting up rook-ceph operator manually due to https://github.com/canonical/microk8s/issues/4314..."
-helm repo add rook-release https://charts.rook.io/release
-helm install rook-ceph rook-release/rook-ceph \
-    --create-namespace \
-    --namespace rook-ceph \
-    --set allowLoopDevices=true 
+# echo "Setting up rook-ceph operator manually due to https://github.com/canonical/microk8s/issues/4314..."
+# helm repo add rook-release https://charts.rook.io/release
+# helm install rook-ceph rook-release/rook-ceph \
+#     --create-namespace \
+#     --namespace rook-ceph \
+#     --set csi.kubeleteDirPath=/var/snap/microk8s/current/var/lib/kubelet \
+#     --set allowLoopDevices=true \
+#     --debug
+#     # --version
 
-helm install rook-ceph-cluster rook-release/rook-ceph-cluster \
-    --create-namespace \
-    --namespace rook-ceph-cluster \
-    --values projects/microceph/rook-ceph/values-external-cluster.yaml 
-    # --values https://raw.githubusercontent.com/rook/rook/master/deploy/charts/rook-ceph-cluster/values-external.yaml
+# helm install rook-ceph-cluster rook-release/rook-ceph-cluster \
+#     --create-namespace \
+#     --namespace rook-ceph-cluster \
+#     --values https://raw.githubusercontent.com/rook/rook/master/deploy/charts/rook-ceph-cluster/values-external.yaml \
+#     --debug
+
+# # Important Notes:
+# # - You must customize the 'CephCluster' resource in the sample manifests for your cluster.
+# # - Each CephCluster must be deployed to its own namespace, the samples use `rook-ceph` for the namespace.
+# # - The sample manifests assume you also installed the rook-ceph operator in the `rook-ceph` namespace.
+# # - The helm chart includes all the RBAC required to create a CephCluster CRD in the same namespace.
+# # - Any disk devices you add to the cluster in the 'CephCluster' must be empty (no filesystem and no partitions).
