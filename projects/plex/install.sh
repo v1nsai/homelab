973475c2a7ac400b0b948602fe6860751f07fd83
doctor_ew <doctor_ew@protonmail.com>
2024-01-23 16:14:59 -0500

    traefik and plex

--- projects/plex/install.sh
+++ projects/plex/install.sh
@@ -13,14 +13,14 @@ kubectl create namespace plex || true
 helm install plex ./projects/plex/kube-plex/charts/kube-plex \
     --namespace plex \
     --set claimToken=$PLEX_CLAIM \
-    --set timezone=America/New_York 
-    # --set service.type=NodePort \
+    --set timezone=America/New_York \
+    --set ingress.enabled=true \
+    --set ingress.hosts[0]=$URL \
+    --set ingress.annotations."kubernetes\.io/ingress\.class"=traefik \
+    --set ingress.annotations."kubernetes\.io/tls-acme"=true 
+    # --set service.type=ClusterIP 
     # --set service.nodePort=32400
     # --set service.externalTrafficPolicy=Local
     # --set persistence.transcode.storageClass=nfs \
     # --set persistence.data.storageClass=nfs \
     # --set persistence.config.storageClass=nfs 
-    # --set ingress.enabled=false \
-    # --set ingress.hosts[0]=$URL \
-    # --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
-    # --set ingress.annotations."kubernetes\.io/tls-acme"=false \
