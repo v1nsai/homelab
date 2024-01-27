248b20b0f68f03d2f6173651620ced8e86f41e28
doctor_ew <doctor_ew@protonmail.com>
2024-01-27 11:12:18 -0500

    nfs provisioner up and running

--- projects/k3s/nfs.sh
+++ projects/k3s/nfs.sh
@@ -3,4 +3,29 @@
 set -e
 
 helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
-helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --namespace kube-system
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
+    --namespace kube-system \
+    --create-namespace \
+    --set externalSnapshotter.enabled=true \
+    --set controller.replicas=2
+
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm upgrade --install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --namespace kube-system \
+    --set nfs.server="192.168.1.198" \
+    --set nfs.path="/mnt/irene" \
+    --kubeconfig /etc/rancher/k3s/k3s.yaml 
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: test-pvc
+spec:
+  accessModes:
+    - ReadWriteMany
+  storageClassName: nfs-client
+  resources:
+    requests:
+      storage: 1Gi
+EOF
