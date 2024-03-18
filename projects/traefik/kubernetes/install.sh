144eb7a558ad4e337b707ee11205be154f7ed789
doctor_ew <doctor_ew@protonmail.com>
2024-03-18 13:17:53 +0000

    changed home-assistant's name, haos troubleshooting

--- projects/traefik/kubernetes/install.sh
+++ projects/traefik/kubernetes/install.sh
@@ -11,4 +11,5 @@ helm upgrade --install traefik traefik/traefik \
     --set certResolvers.letsencrypt-prod.httpChallenge.entryPoint="web" \
     --set certResolvers.letsencrypt-staging.email="${EMAIL}" \
     --set certResolvers.letsencrypt-staging.storage="/data/acme.json" \
-    --set certResolvers.letsencrypt-staging.httpChallenge.entryPoint="web"
+    --set certResolvers.letsencrypt-staging.httpChallenge.entryPoint="web" \
+    --set certResolvers.letsencrypt-staging.caServer="https://acme-staging-v02.api.letsencrypt.org/directory"
