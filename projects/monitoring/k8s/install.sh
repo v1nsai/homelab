d35a25c7cf5514d5ea0c1e2aeaf7a2db078436aa
doctor_ew <doctor_ew@protonmail.com>
2024-05-15 18:45:53 -0400

    cleanup monitoring folder

--- projects/monitoring/k8s/install.sh
+++ projects/monitoring/k8s/install.sh
@@ -13,10 +13,3 @@ kubectl wait \
 kubectl apply -f projects/monitoring/k8s/kube-prometheus/manifests/
 kubectl patch svc grafana -n monitoring --type='json' -p '[{"op":"replace","path":"/spec/type","value":"LoadBalancer"}]'
 # kubectl delete --ignore-not-found=true -f projects/monitoring/k8s/kube-prometheus/manifests/ -f projects/monitoring/k8s/kube-prometheus/manifests/setup
-
-echo "Installing grafana..."
-helm upgrade --install grafana-operator oci://ghcr.io/grafana/helm-charts/grafana-operator \
-    --create-namespace \
-    --namespace monitoring
-kubectl apply -f projects/monitoring/k8s/grafana/grafana.yaml -f projects/monitoring/k8s/grafana/datasources.yaml -f projects/monitoring/k8s/grafana/dashboards.yaml -n monitoring
-kubectl patch svc grafana-service -n monitoring --type='json' -p '[{"op":"replace","path":"/spec/type","value":"LoadBalancer"}]'
