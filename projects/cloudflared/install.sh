3f0796d7dd019796009fcf5cfd89599cf3580776
doctor_ew <doctor_ew@protonmail.com>
2024-05-18 17:53:59 -0400

    cloudflare tunnels deployed to k8s

--- projects/cloudflared/install.sh
+++ projects/cloudflared/install.sh
@@ -4,19 +4,13 @@ set -e
 source projects/cloudflared/.env
 # To create a new tunnel, follow official docs https://developers.cloudflare.com/cloudflare-one/tutorials/many-cfd-one-tunnel/#install-cloudflared
 
-# Use an existing tunnel created in the UI
-# get this info by going to existing tunnel in UI, going to configure, selecting Docker and copying the token after --token
-# then go to jwt.io and paste the token in the debugger
-# a = AccountTag, t = TunnelID, s = TunnelSecret
+# Use an existing tunnel created in the UI, use cloudflare-tunnel-remote chart.  For local administration, use cloudflare-tunnel
 helm repo add cloudflare https://cloudflare.github.io/helm-charts
 helm repo update
-helm upgrade --install cloudflared cloudflare/cloudflare-tunnel \
+helm upgrade --install cloudflared cloudflare/cloudflare-tunnel-remote \
     --namespace cloudflare \
     --create-namespace \
-    --set cloudflare.account=$CLOUDFLARED_ACCOUNT_TAG \
-    --set cloudflare.tunnelName=homelab \
-    --set cloudflare.tunnelId=$CLOUDFLARED_TUNNEL_ID \
-    --set cloudflare.secret=$CLOUDFLARED_TUNNEL_SECRET 
+    --set cloudflare.tunnel_token=$CF_TUNNEL_TOKEN
 
 # CLOUDFLARED_TUNNEL_JSON="projects/cloudflared/k8s/cloudflared.json.env"
 # cat > $CLOUDFLARED_TUNNEL_JSON <<- EOM
