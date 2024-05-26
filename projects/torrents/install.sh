a35c008fd37e3507dc9f31116209df60a4a57e3d
doctor_ew <doctor_ew@protonmail.com>
2024-05-26 11:51:50 -0400

    fixed torrents to work properly with loadbalancer

--- projects/torrents/install.sh
+++ projects/torrents/install.sh
@@ -2,13 +2,24 @@
 
 set -e
 
+# Delete first for troubleshooting
+# kubectl delete secret wireguard-config \
+#     --namespace plex || true
+# kubectl delete \
+#     --namespace plex \
+#     --filename projects/torrents/torrents.yaml || true
+
 kubectl create secret generic wireguard-config \
     --namespace plex \
-    --from-file=projects/torrents/config/gluetun/wg0.conf
+    --from-file=projects/torrents/config/gluetun/wg0.conf || true
+
 kubectl apply \
     --namespace plex \
     --filename projects/torrents/torrents.yaml
+
 # Reboot qbittorrent container since it can't connect to network if both are started at the same time
+echo "Sleeping and then restarting qbittorrent container..."
+sleep 10
 kubectl exec -it \
     --namespace plex \
     $(kubectl get pods \
