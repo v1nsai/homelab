c23b76e560a9670aaffe0db2aef6129623e4ceff
doctor_ew <doctor_ew@protonmail.com>
2024-05-30 11:26:47 -0400

    better plex install script, fixes

--- projects/plex/install-pms.sh
+++ projects/plex/install-pms.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 set -e
-source projects/plex/.env
+# source projects/plex/.env
 
-if [ $(kubectl get pvc -n plex plex-media | wc -l) -lt 2 ]; then
-    kubectl apply -f projects/plex/plex-media-pvc.yaml
+read -p "Delete existing installation? (y/N)" DELETE
+if [ "$DELETE" == "y" ]; then
+    helm delete plex -n plex
+    kubectl delete -n plex pvc pms-config-plex-plex-media-server-0
+    read -p "Enter current plex claim token: " PLEX_CLAIM
 fi
-
-echo $PLEX_CLAIM
 helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
 helm upgrade --install plex plex/plex-media-server \
     --create-namespace \
     --namespace plex \
-    --set image.pullPolicy=Always \
     --set service.type=LoadBalancer \
     --set extraVolumeMounts[0].name=plex-media \
     --set extraVolumeMounts[0].mountPath=/plex-media \
     --set extraVolumes[0].name=plex-media \
     --set extraVolumes[0].persistentVolumeClaim.claimName=plex-media \
     --set extraEnv.PLEX_CLAIM="$PLEX_CLAIM"
+
+
+    # --set extraVolumeMounts[1].name=media-pvc \
+    # --set extraVolumeMounts[1].mountPath=/media-pvc \
+    # --set extraVolumes[1].name=media-pvc \
+    # --set extraVolumes[1].persistentVolumeClaim.claimName=media-pvc \
