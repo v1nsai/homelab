f89f40f8e55434c52685e384afbb63c3bb96d557
doctor_ew <doctor_ew@protonmail.com>
2024-06-05 19:43:23 -0400

    cleanup and minor changes

--- projects/nfs/install.sh
+++ projects/nfs/install.sh
@@ -9,33 +9,21 @@ helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
     --set externalSnapshotter.enabled=true \
     --set controller.replicas=2
 
+# oppenheimer
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 # helm upgrade --install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
 #     --namespace kube-system \
 #     --set nfs.server="192.168.1.152" \
 #     --set nfs.path="/mnt/silverstick/kubernetes"
 
-# for bigrig
+# bigrig
 helm upgrade --install irma-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --create-namespace \
     --namespace irma-provisioner \
     --set nfs.server="192.168.1.169" \
     --set nfs.path="/mnt/irma/kubernetes" \
-    --set storageClass.name="irma"
-
-kubectl apply -f - <<EOF
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: test-pvc
-spec:
-  accessModes:
-    - ReadWriteMany
-  storageClassName: irma
-  resources:
-    requests:
-      storage: 1Gi
-EOF
+    --set storageClass.name="irma" \
+    --set storageClass.provisionerName="k8s-sigs.io/irma-nfs-subdir-external-provisioner"
 
 echo "/mnt/silverstick *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -a
