606efcb349f43409417af68529138b19f34a5055
doctor_ew <doctor_ew@protonmail.com>
2024-06-16 21:49:26 -0400

    velero cleanup

--- projects/velero/install.sh
+++ projects/velero/install.sh
@@ -16,7 +16,6 @@ fi
 # echo 'source <(velero completion bash)' >>~/.bashrc
 
 echo "Configuring AWS IAM user..."
-aws iam create-user --user-name velero
 cat > projects/velero/velero-policy.json <<EOF
 {
     "Version": "2012-10-17",
@@ -58,6 +57,12 @@ cat > projects/velero/velero-policy.json <<EOF
     ]
 }
 EOF
+cat > projects/velero/velero.env <<EOF
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+EOF
+aws iam create-user --user-name velero
 aws iam put-user-policy \
   --user-name velero \
   --policy-name velero \
@@ -65,14 +70,11 @@ aws iam put-user-policy \
 aws iam create-access-key --user-name velero > ~/.aws/velero-user.json
 AWS_SECRET_ACCESS_KEY=$(jq -r '.AccessKey.SecretAccessKey' ~/.aws/velero-user.json)
 AWS_ACCESS_KEY_ID=$(jq -r '.AccessKey.AccessKeyId' ~/.aws/velero-user.json)
-cat > projects/velero/velero.env <<EOF
-[default]
-aws_access_key_id=${AWS_ACCESS_KEY_ID}
-aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
-EOF
-
 REGION=$(aws configure get region)
+
+echo "Installing velero..."
 velero install \
+    # --use-restic \
     --provider aws \
     --plugins velero/velero-plugin-for-aws:v1.9.2 \
     --bucket $BUCKET \
@@ -81,13 +83,21 @@ velero install \
     --secret-file projects/velero/velero.env
 
 echo "Setting up volume backup exclusions..."
-kubectl -n plex annotate pod/plex-plex-media-server-0 backup.velero.io/backup-volumes-excludes=the-goods,plex-media
+kubectl -n jellyfin annotate pod/plex-plex-media-server-0 backup.velero.io/backup-volumes-excludes=the-goods
 
 echo "Scheduling backups..."
 velero schedule create nightly --schedule="0 3 * * *" --ttl 168h0m0s
 
+echo "Adding backup rules..."
+kubectl apply -f projects/velero/change-storageclass.yaml
+
 # restore a namespace from backup
-NAMESPACE="nextcloud"
-RESTORE_DATE=$(date -d "2 days ago" +%Y%m%d)
-BACKUP_NAME=$(aws s3 ls s3://$BUCKET/backups/ | grep $RESTORE_DATE | awk '{print $2}' | tr -d "/")
-velero restore create --from-backup $BACKUP_NAME --include-namespaces $NAMESPACE
+# NAMESPACE="nextcloud"
+# RESTORE_DATE=$(date -d "1 days ago" +%Y%m%d)
+# BACKUP_NAME=$(aws s3 ls s3://$BUCKET/backups/ | grep $RESTORE_DATE | awk '{print $2}' | tr -d "/")
+# velero restore create --from-backup $BACKUP_NAME --include-namespaces $NAMESPACE
+
+# velero backup create torrents-$(date +%Y%m%d) --include-namespaces plex --include-resources persistentvolumeclaims
+# velero restore create torrents-plex --from-backup torrents-$(date +%Y%m%d) --namespace  #--namespace-mappings plex:jellyfin
+
+velero backup create nextcloud-test --include-namespaces nextcloud
