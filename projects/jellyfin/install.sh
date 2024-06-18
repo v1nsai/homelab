222c53fd8ba5a88fb368a528a2e3de03a0d36e40
doctor_ew <doctor_ew@protonmail.com>
2024-06-18 13:58:35 -0400

    switched jellyfin config storage to hostpath

--- projects/jellyfin/install.sh
+++ projects/jellyfin/install.sh
@@ -4,7 +4,6 @@ set -e
 
 # git clone git@github.com:jellyfin/jellyfin-helm.git projects/jellyfin/jellyfin-helm
 
-# bash command prompt user "Delete deployment and pvcs? [y/n]" and put it into a variable called DELETE
 read -p "Delete deployment and non-media pvcs? [y/N]" DELETE
 if [ "$DELETE" == "y" ]; then
     helm delete jellyfin --namespace jellyfin || true
@@ -15,7 +14,3 @@ helm upgrade --install jellyfin ./projects/jellyfin/jellyfin-helm/charts/jellyfi
     --namespace jellyfin \
     --create-namespace \
     --values projects/jellyfin/values.yaml
-
-kubectl exec -it $(kubectl get pods -n jellyfin | grep jellyfin | awk '{print $1}') -n jellyfin -- /bin/bash -c 'echo "0 3 * * * rsync -av --delete /config /media/jellyfin-config-backup" > /etc/cron.d/jellyfin-config-backup'
-
-# check for file lock on /config/data/jellyfin.db sqlite database
