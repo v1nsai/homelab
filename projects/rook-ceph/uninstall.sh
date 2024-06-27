9b948d5f105cafafcc378aae8ba025fe736e4b28
doctor_ew <doctor_ew@protonmail.com>
2024-06-27 17:45:33 -0400

    apparently there are 2 places to specify kubeletdir

--- projects/rook-ceph/uninstall.sh
+++ projects/rook-ceph/uninstall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-helm delete -n rook-ceph rook-ceph || true
-helm delete -n rook-cluster rook-ceph-cluster || true
-kubectl delete namespace rook-ceph &
+# helm delete -n rook-ceph rook-ceph
+# helm delete -n rook-cluster rook-ceph-cluster
 
 echo "Removing CRs and CRDs..."
 kubectl -n rook-ceph patch cephcluster rook-ceph --type merge -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
@@ -20,11 +19,7 @@ kubectl -n rook-ceph patch secrets rook-ceph-mon --type merge -p '{"metadata":{"
 # kubectl -n rook-ceph annotate cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup rook.io/force-deletion="true"
 # kubectl -n rook-ceph delete cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup
 
-echo "Waiting for namespace to be deleted..."
-while kubectl get namespace rook-ceph &> /dev/null; do
-    echo "Namespace still exists, waiting 5s and trying again..."
-    sleep 5
-done
+kubectl delete namespace rook-ceph --wait
 
 echo "Wiping ceph OSDs and deleting config files on hosts..."
 echo "Wiping bigrig OSDs..."
@@ -33,12 +28,11 @@ sudo sgdisk --zap-all $DISK
 sudo dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
 sudo blkdiscard $DISK
 sudo partprobe $DISK
-
 sudo rm -rf /var/lib/rook
 
 echo "Wiping oppenheimer OSDs..."
+DISK="/dev/sdc"
 ssh oppenheimer /bin/bash << EOF
-    DISK="/dev/sdc"
     sudo sgdisk --zap-all $DISK
     sudo dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
     sudo blkdiscard $DISK
