e7195f060598b2b5b370b5fda2b638a0c639f049
drew <drew@doctor-ew.com>
2024-07-06 17:42:15 +0000

    utility scripts

---
+++ scripts/delete-stuck-namespace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+
+echo "Deleting remaining resources in $1..."
+export NAMESPACE="$1"
+kubectl delete namespace $NAMESPACE --wait=false --grace-period=0 --force
+kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n $NAMESPACE | awk '{print $1}' | xargs -n 1 kubectl delete -n $NAMESPACE --grace-period=0 --force
+
+kubectl get ns $NAMESPACE -ojson | jq '.spec.finalizers = []' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
+
+# echo "Removing finalizers from namespace $NAMESPACE..."
+# kubectl get namespace -o json $NAMESPACE > stuck.json
+# jq '.spec.finalizers = []' stuck.json > stuck.nofinalizers.json
+# kubectl proxy &
+# PROXY_PID=$!
+# curl -k -H "Content-Type: application/json" -X PUT --data-binary @stuck.nofinalizers.json http://127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+
+# echo "Cleaning up..."
+# kill $PROXY_PID
+# rm stuck.json
