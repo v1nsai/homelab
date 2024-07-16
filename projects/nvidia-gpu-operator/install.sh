e2d28082d512d82c8387ed5d62c70788864b0a73
drew <drew@doctor-ew.com>
2024-07-16 23:45:33 +0000

    deploy harbor

--- projects/nvidia-gpu-operator/install.sh
+++ projects/nvidia-gpu-operator/install.sh
@@ -11,3 +11,6 @@ helm upgrade --install nvidia-gpu-operator nvidia/gpu-operator \
 
 # sudo vim /var/snap/microk8s/current/args/containerd.toml
 # sudo systemctl restart snap.microk8s.daemon-containerd.service
+
+# ignore tiffrig since GPU is too old
+kubectl label node tiffrig nvidia.com/gpu.deploy.operands=false
