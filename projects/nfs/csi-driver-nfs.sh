56b49f99db30159229ade0e6aca11191c47970ca
drew <drew@doctor-ew.com>
2024-07-20 23:45:30 +0000

    nfs snapshots

--- projects/nfs/csi-driver-nfs.sh
+++ projects/nfs/csi-driver-nfs.sh
@@ -5,7 +5,6 @@ set -e
 helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
 helm repo update
 helm upgrade --install irma-csi-driver-nfs csi-driver-nfs/csi-driver-nfs \
-    --namespace kube-system \
+    --namespace irma-csi-driver-nfs \
     --create-namespace \
-    --version v4.7.0 \
     --values projects/nfs/csi-driver.values.yaml
