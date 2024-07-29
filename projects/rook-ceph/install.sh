c2aafad8f4a2e6f30207df84ba5f7b88d33f7d06
doctor_ew <doctor_ew@protonmail.com>
2024-07-28 21:58:56 -0400

    ceph troubleshooting

--- projects/rook-ceph/install.sh
+++ projects/rook-ceph/install.sh
@@ -10,12 +10,15 @@ if [[ $delete == [yY] ]]; then
 fi
 
 helm repo add rook-release https://charts.rook.io/release
+helm repo update
+cat projects/rook-ceph/operator/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph.values.yaml
 helm upgrade --install rook-ceph rook-release/rook-ceph \
     --create-namespace \
     --namespace rook-ceph \
-    --values projects/rook-ceph/rook-ceph.values.yaml
+    --values /tmp/rook-ceph.values.yaml
 
+cat projects/rook-ceph/cluster/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph-cluster.values.yamls
 helm upgrade --install rook-ceph-cluster rook-release/rook-ceph-cluster \
     --create-namespace \
     --namespace rook-ceph \
-    --values projects/rook-ceph/rook-ceph-cluster.values.yaml
+    --values /tmp/rook-ceph-cluster.values.yamls
---
+++ projects/talos/extensions/nvidia/install.sh
@@ -0,0 +1,33 @@
+# nvidia
+talosctl upgrade \
+  --nodes 192.168.1.170 \
+  --endpoints 192.168.1.170 \
+  --image factory.talos.dev/installer/aefe418d4647eb3ecb93d2c5d583c663aa54c790165493b0414bf01442d93897:v1.7.5
+  # --image ghcr.io/siderolabs/installer:v1.7.5
+  # --reboot-mode powercycle
+  # --stage
+talosctl patch machineconfig \
+  --nodes 192.168.1.170 \
+  --endpoints 192.168.1.170 \
+  --patch-file projects/talos/nvidia/gpu-worker-patch.yaml
+
+## allow running privileged containers in nvidia namespace
+kubectl apply -f projects/talos/nvidia/runtimeclass.yaml
+kubectl create ns nvidia-device-plugin
+kubectl label --overwrite namespace nvidia-device-plugin \
+    pod-security.kubernetes.io/enforce=privileged \
+    pod-security.kubernetes.io/enforce-version=latest \
+    pod-security.kubernetes.io/warn=privileged \
+    pod-security.kubernetes.io/warn-version=latest \
+    pod-security.kubernetes.io/audit=privileged \
+    pod-security.kubernetes.io/audit-version=latest
+
+## label GPU nodes
+kubectl label nodes bigrig nvidia.com/gpu.present=true
+
+helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+helm repo update
+helm upgrade --install nvidia-device-plugin nvdp/nvidia-device-plugin \
+    --create-namespace \
+    --namespace nvidia-device-plugin \
+    --set=runtimeClassName=nvidia
