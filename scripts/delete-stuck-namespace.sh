a2d9b2019efe41c54ff92dbb92721ba67587e5f8
doctor_ew <doctor_ew@protonmail.com>
2024-07-29 16:36:09 -0400

    backup troubleshooting

--- scripts/delete-stuck-namespace.sh
+++ scripts/delete-stuck-namespace.sh
@@ -3,24 +3,24 @@
 set -e
 
 
-echo "Deleting remaining resources in $1..."
-export NAMESPACE="$1"
+if [ -z "$1" ]; then
+    echo "Deleting all namespaces in Terminating state..."
+    NAMESPACES=($(kubectl get ns --no-headers | awk '$2=="Terminating" {print $1}'))
+    for NAMESPACE in "${NAMESPACES[@]}"; do
+        echo "Deleting namespace $NAMESPACE..."
+        kubectl get ns $NAMESPACE -ojson | jq '.spec.finalizers = []' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
+    done
+    exit 0
+fi
+
+NAMESPACE=$1
 
 # list all resources in namespace
-# kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n $NAMESPACE
-# TODO add some awk/sed fu to automate deleting all the resources from the previous command
-kubectl get ns $NAMESPACE -ojson | jq '.spec.finalizers = []' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
+kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n $NAMESPACE
 
-# echo "Removing finalizers from namespace $NAMESPACE..."
-# kubectl get namespace -o json $NAMESPACE > stuck.json
-# jq '.spec.finalizers = []' stuck.json > stuck.nofinalizers.json
-# kubectl proxy &
-# PROXY_PID=$!
-# curl -k -H "Content-Type: application/json" -X PUT --data-binary @stuck.nofinalizers.json http://127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
-
-# echo "Cleaning up..."
-# kill $PROXY_PID
-# rm stuck.json
-
-# kubectl delete namespace $NAMESPACE --wait=false --grace-period=0 --force
-# kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found -n $NAMESPACE | awk '{print $1}' | xargs -n 1 kubectl delete -n $NAMESPACE --grace-period=0 --force
+read -sn1 -p "Force delete namespace $NAMESPACE? [y/N] " CONFIRM
+if [ "$CONFIRM" == "yY" ]; then
+    echo "Deleting namespace $NAMESPACE..."
+    kubectl get ns $NAMESPACE -ojson | jq '.spec.finalizers = []' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
+    exit 0
+fi
