33f3fb9f2a20ed8030f0a331b72240f67275d995
doctor_ew <doctor_ew@protonmail.com>
2024-07-30 16:57:03 -0400

    flux install script cleanup

--- projects/fluxcd/install.sh
+++ projects/fluxcd/install.sh
@@ -4,7 +4,7 @@ set -e
 source projects/fluxcd/fluxcd.env
 
 echo "Installing fluxcd..."
-if [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_USER" ] || [ -z "$GITHUB_REPO" ]; then
+if [ -z "$GITHUB_USER" ] || [ -z "$GITHUB_REPO" ]; then
   echo "GITHUB_TOKEN, GITHUB_USER and GITHUB_REPO must be set in projects/fluxcd/secrets.env"
   exit 1
 fi
@@ -20,8 +20,8 @@ flux bootstrap github \
 echo "Installing weave gitops UI..."
 if [ -z "$WW_DASH_PASS" ]; then
     WW_DASH_PASS="$(openssl rand -base64 20)"
-    echo "$WW_DASH_PASS" | tee -a ./projects/fluxcd/fluxcd.env
+    echo "WW_DASH_PASS='$WW_DASH_PASS'" | tee -a ./projects/fluxcd/fluxcd.env
 fi
 gitops create dashboard ww-gitops \
   --password=$WW_DASH_PASS \
-  --export > ./projects/fluxcd/flux-system/weave-gitops.yaml
+  --export > ./projects/fluxcd/extensions/weave-gitops.yaml
