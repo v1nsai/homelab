387f94bc8e5c61e6fc46fad6f1cce13722d2e59d
doctor_ew <doctor_ew@protonmail.com>
2024-08-01 19:29:53 -0400

    bump velero server version

--- scripts/flux-reconcile.sh
+++ scripts/flux-reconcile.sh
@@ -4,3 +4,13 @@ set -e
 
 flux reconcile source git homelab
 flux reconcile kustomization add-projects-folder
+
+APPNAME="$1"
+if [ ! -z "$APPNAME" ]; then
+  NAMESPACE="$2"
+    if [ -z "$NAMESPACE" ]; then
+        NAMESPACE=$APPNAME
+    fi
+  flux reconcile -n $NAMESPACE kustomization $APPNAME
+  flux reconcile -n $NAMESPACE helmrelease $APPNAME
+fi
