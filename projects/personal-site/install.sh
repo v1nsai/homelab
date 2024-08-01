ebf05633c4e74704b2ef744a9b5261c1d49d0a4c
doctor_ew <doctor_ew@protonmail.com>
2024-08-01 16:23:27 -0400

    deploy wordpress with flux

--- projects/personal-site/install.sh
+++ projects/personal-site/install.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-set -e
-source projects/personal-site/secrets.env
-
-# if [ -z "$WORDPRESS_PASSWORD" ] || [ -z "$MARIADB_PASSWORD" ]; then
-#     echo "Please set WORDPRESS_PASSWORD and MARIADB_PASSWORD in projects/personal-site/secrets.env"
-#     exit 1
-# fi
-
-read -sn1 -p "Delete before installing? [y/N] " DELETE
-if [ "$DELETE" == "y" ] || [ "$DELETE" == "Y" ]; then
-    echo "" 
-    helm delete -n personal-site personal-site || true
-    kubectl delete ns personal-site || true
-    kubectl create ns personal-site || true
-fi
-
 kubectl create secret generic wordpress-password \
     --namespace personal-site \
-    --from-literal=wordpress-password=$(openssl rand -base64 20) || true
+    --from-literal=wordpress-password=$(openssl rand -base64 20) \
+    --dry-run=client \
+    --output yaml | \
+kubeseal --format yaml --cert ./.sealed-secrets.pub > projects/personal-site/app/sealed-secrets.yaml
 
 kubectl create secret generic externaldb-password \
     --namespace personal-site \
-    --from-literal=mariadb-password=$(openssl rand -base64 20) || true
-
-helm delete -n personal-site personal-site || true
-helm upgrade --install personal-site oci://registry-1.docker.io/bitnamicharts/wordpress \
-    --namespace personal-site \
-    --set wordpressUsername=doctor_ew \
-    --set existingSecret=wordpress-password \
-    --set externalDatabase.existingSecret=externaldb-password 
-    # --set nodeSelector."kubernetes\.io/hostname"=bigrig
-    # --set global.storageClass=microk8s-hostpath
+    --from-literal=mariadb-password=$(openssl rand -base64 20) \
+    --dry-run=client \
+    --output yaml | \
+kubeseal --format yaml --cert ./.sealed-secrets.pub | tee -a projects/personal-site/app/sealed-secrets.yaml
