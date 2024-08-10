58f332e91ebc1ead8b149d11ddea0aa8ce81c767
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 18:18:59 -0400

    generate selfsigned sealed secret script

--- scripts/generate-selfsigned.sh
+++ scripts/generate-selfsigned.sh
@@ -2,52 +2,6 @@
 
 set -e
 
-# Parse arguments
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -u|--url)
-      URL="$2"
-      shift
-      shift
-      ;;
-    -n|--namespace)
-      NAMESPACE="$2"
-      shift
-      shift
-      ;;
-    -p|--projectname)
-      PROJECTNAME="$2"
-      shift
-      shift
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
-      ;;
-    *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift
-      ;;
-  esac
-done
-set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
-
-# Ensure required arguments are set
-if [ -z "$PROJECTNAME" ]; then
-  PROJECTNAME="$NAMESPACE"
-fi
-if [ -z "$NAMESPACE" ]; then
-  NAMESPACE="$PROJECTNAME"
-fi
-if [ -z "$URL" ]; then
-  echo "URL is required, defaulting to '*'"
-  URL="*"
-fi
-if [ -z "$PROJECTNAME" ] && [ -z "$NAMESPACE" ]; then
-  echo "Project name or namespace is required"
-  exit 1
-fi
-
 # Generate self-signed cert
 openssl req \
     -x509 \
@@ -56,7 +10,7 @@ openssl req \
     -newkey rsa:2048 \
     -keyout /tmp/tls.key \
     -out /tmp/tls.crt \
-    -subj "/CN=$URL"
+    -subj "/CN=*"
 
 # Create k8s secret yaml and seal it
 kubectl create secret tls selfsigned-tls \
@@ -65,7 +19,7 @@ kubectl create secret tls selfsigned-tls \
     --namespace $NAMESPACE \
     --dry-run=client \
     --output yaml | \
-kubeseal --format=yaml --cert=./.sealed-secrets.pub > projects/$PROJECTNAME/app/selfsigned-tls-sealed.yaml
+kubeseal --format=yaml --cert=./.sealed-secrets.pub > sealed-secret.yaml
 
 # Cleanup
 rm /tmp/tls.key /tmp/tls.crt 
