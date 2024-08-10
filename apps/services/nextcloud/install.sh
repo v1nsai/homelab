5ee795be329e42d51f8b87c384d0667c0b33e607
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 19:52:26 -0400

    fix nextcloud typo

--- apps/services/nextcloud/install.sh
+++ apps/services/nextcloud/install.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Generating or retrieving credentials..."
-source apps/services/secrets.env
+source apps/services/nextcloud/secrets.env
 
 if [ -z "$NC_HOST" ]; then
     read -sn1 -p "Enter Nextcloud host: " NC_HOST
-    cat "NC_HOST=$NC_HOST" >> apps/services/secrets.env
+    cat "NC_HOST=$NC_HOST" >> apps/services/nextcloud/secrets.env
     export NC_HOST="$NC_HOST"
 fi
 
@@ -22,8 +22,8 @@ kubectl create secret -n nextcloud generic nextcloud-admin \
     --from-literal=nextcloud-username=admin \
     --from-literal=nextcloud-token=$NC_PASSWORD \
     --dry-run=client \
-    --output=yaml > apps/services/secret.yaml
-kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/secret.yaml | tee -a apps/services/app/sealed-secrets.yaml
+    --output=yaml > apps/services/nextcloud/secret.yaml
+kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/nextcloud/secret.yaml | tee -a apps/services/nextcloud/app/sealed-secrets.yaml
 
 echo "Generating mariadb passwords..."
 echo "Generating mariadb-passwords..."
@@ -38,8 +38,8 @@ kubectl create secret -n nextcloud generic mariadb-passwords \
     --from-literal=mariadb-replication-password=$MARIADB_REPLICATION_PASSWORD \
     --from-literal=mariadb-username=$MARIADB_USERNAME \
     --dry-run=client \
-    --output=yaml > apps/services/secret.yaml
-kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/secret.yaml | tee -a apps/services/app/sealed-secrets.yaml
+    --output=yaml > apps/services/nextcloud/secret.yaml
+kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/nextcloud/secret.yaml | tee -a apps/services/nextcloud/app/sealed-secrets.yaml
 
 echo "Generating redis credentials..."
 echo "Generating redis-password..."
@@ -47,8 +47,8 @@ REDIS_PASSWORD=$(openssl rand -base64 20)
 kubectl create secret -n nextcloud generic redis-password \
     --from-literal=redis-password=$REDIS_PASSWORD \
     --dry-run=client \
-    --output=yaml > apps/services/secret.yaml
-kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/secret.yaml | tee -a apps/services/app/sealed-secrets.yaml
+    --output=yaml > apps/services/nextcloud/secret.yaml
+kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/nextcloud/secret.yaml | tee -a apps/services/nextcloud/app/sealed-secrets.yaml
 
 echo "Creating self signed certs..."
 echo "Generating selfsigned-tls..."
@@ -59,8 +59,8 @@ kubectl create secret -n nextcloud tls selfsigned-tls \
     --cert=/tmp/nextcloud.crt \
     --key=/tmp/nextcloud.key \
     --dry-run=client \
-    --output=yaml > apps/services/secret.yaml
-kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/secret.yaml | tee -a apps/services/app/sealed-secrets.yaml
+    --output=yaml > apps/services/nextcloud/secret.yaml
+kubeseal --format=yaml --cert=./.sealed-secrets.pub < apps/services/nextcloud/secret.yaml | tee -a apps/services/nextcloud/app/sealed-secrets.yaml
 
 echo "Configuring secret values..."
 cat << EOF > /tmp/secret-values.yaml
@@ -71,7 +71,7 @@ kubectl create secret generic -n nextcloud secret-values \
     --from-file=/tmp/secret-values.yaml \
     --dry-run=client \
     --output yaml | \
-kubeseal --cert ./.sealed-secrets.pub --format yaml | tee -a apps/services/app/sealed-secrets.yaml
+kubeseal --cert ./.sealed-secrets.pub --format yaml | tee -a apps/services/nextcloud/app/sealed-secrets.yaml
 
 rm -rf /tmp/nextcloud.key /tmp/nextcloud.crt /tmp/secret-values.yaml
-rm -rf apps/services/secret.yaml
+rm -rf apps/services/nextcloud/secret.yaml
