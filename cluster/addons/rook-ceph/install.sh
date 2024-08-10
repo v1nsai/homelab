b77c64d7e9fb6bb54c4917fd925ed4da4667a29f
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 18:11:02 -0400

    mv rook-ceph directory

--- cluster/addons/rook-ceph/install.sh
+++ cluster/addons/rook-ceph/install.sh
@@ -6,13 +6,13 @@ set -e
 
 helm repo add rook-release https://charts.rook.io/release
 helm repo update
-cat projects/rook-ceph/operator/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph.values.yaml
+cat cluster/addons/rook-ceph/operator/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph.values.yaml
 helm upgrade --install rook-ceph rook-release/rook-ceph \
     --create-namespace \
     --namespace rook-ceph \
     --values /tmp/rook-ceph.values.yaml
 
-cat projects/rook-ceph/cluster/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph-cluster.values.yaml
+cat cluster/addons/rook-ceph/cluster/helmrelease.yaml | yq '.spec.values' > /tmp/rook-ceph-cluster.values.yaml
 helm upgrade --install rook-ceph-cluster rook-release/rook-ceph-cluster \
     --create-namespace \
     --namespace rook-ceph \
