4e24fe2a86d0ab9e8bc009996d88224c742becc2
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 17:43:47 -0400

    mv longhorn directory

--- cluster/addons/longhorn/config/backups.sh
+++ cluster/addons/longhorn/config/backups.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 set -e
-source projects/longhorn/.env
+source cluster/addons/longhorn/.env
 REGION=$(aws configure get region)
 
 if [ -z "$BUCKET" ] || [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-    echo "Please create an IAM user and set the BUCKET, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in projects/longhorn/.env"
+    echo "Please create an IAM user and set the BUCKET, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in cluster/addons/longhorn/.env"
     exit 1
 fi
 
 echo "Creating backup location secrets..."
-source projects/longhorn/.env
+source cluster/addons/longhorn/.env
 kubectl create secret generic backup-target-credentials \
     --namespace longhorn-system \
     --from-literal AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
     --from-literal AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
     --dry-run=client \
-    --output yaml | kubeseal --cert ./.sealed-secrets.pub --format yaml > projects/longhorn/app/sealed-secrets.yaml
+    --output yaml | kubeseal --cert ./.sealed-secrets.pub --format yaml > cluster/addons/longhorn/app/sealed-secrets.yaml
