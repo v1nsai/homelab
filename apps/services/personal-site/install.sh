1333d19dfa82c81535f635eb6953c490390bcaaf
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 17:51:52 -0400

    mv personal-site directory

--- apps/services/personal-site/install.sh
+++ apps/services/personal-site/install.sh
@@ -5,11 +5,11 @@ kubectl create secret generic wordpress-password \
     --from-literal=wordpress-password=$(openssl rand -base64 20) \
     --dry-run=client \
     --output yaml | \
-kubeseal --format yaml --cert ./.sealed-secrets.pub > projects/personal-site/app/sealed-secrets.yaml
+kubeseal --format yaml --cert ./.sealed-secrets.pub > apps/services/personal-site/app/sealed-secrets.yaml
 
 kubectl create secret generic externaldb-password \
     --namespace personal-site \
     --from-literal=mariadb-password=$(openssl rand -base64 20) \
     --dry-run=client \
     --output yaml | \
-kubeseal --format yaml --cert ./.sealed-secrets.pub | tee -a projects/personal-site/app/sealed-secrets.yaml
+kubeseal --format yaml --cert ./.sealed-secrets.pub | tee -a apps/services/personal-site/app/sealed-secrets.yaml
