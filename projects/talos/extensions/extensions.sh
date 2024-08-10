c5f752bb596e00da72daf92eb73d554ddfcc7c5d
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 16:12:32 -0400

    downgrade to talos 1.7.5 to fix dhcp issue

--- projects/talos/extensions/extensions.sh
+++ projects/talos/extensions/extensions.sh
@@ -9,18 +9,18 @@ set -e
 talosctl upgrade \
   --nodes 192.168.1.170 \
   --endpoints 192.168.1.170 \
-  --image factory.talos.dev/installer/8f8a8d4be4bde03c257e0cac6274c567053a5ed8dfa0a00ce234abef61e5067d:v1.7.6
+  --image factory.talos.dev/installer/8f8a8d4be4bde03c257e0cac6274c567053a5ed8dfa0a00ce234abef61e5067d:v1.7.5
 
 # tiffrig
 # iscsi-tools util-linux-tools 
 talosctl upgrade \
   --nodes 192.168.1.155 \
   --endpoints 192.168.1.155 \
-  --image factory.talos.dev/installer/613e1592b2da41ae5e265e8789429f22e121aab91cb4deb6bc3c0b6262961245:v1.7.6
+  --image factory.talos.dev/installer/613e1592b2da41ae5e265e8789429f22e121aab91cb4deb6bc3c0b6262961245:v1.7.4
 
 # oppenheimer
 # iscsi-tools util-linux-tools 
 talosctl upgrade \
     --nodes 192.168.1.162 \
     --endpoints 192.168.1.162 \
-    --image factory.talos.dev/installer/613e1592b2da41ae5e265e8789429f22e121aab91cb4deb6bc3c0b6262961245:v1.7.6
+    --image factory.talos.dev/installer/613e1592b2da41ae5e265e8789429f22e121aab91cb4deb6bc3c0b6262961245:v1.7.5
