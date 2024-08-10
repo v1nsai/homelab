58e77d576039688f036f42bad571b9a08de4e038
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 18:12:19 -0400

    mv torrents directory

--- apps/services/torrents/install.sh
+++ apps/services/torrents/install.sh
@@ -8,8 +8,8 @@ if [ "$RECREATE_SECRET" == "y" ]; then
     #     --namespace jellyfin || true
     kubectl create secret generic wireguard-config \
         --namespace jellyfin \
-        --from-file=projects/torrents/secrets/gluetun/wg0.conf \
+        --from-file=apps/services/torrents/secrets/gluetun/wg0.conf \
         --dry-run=client \
         -o yaml | \
-    kubeseal --format=yaml --cert=./.sealed-secrets.pub > projects/torrents/app/wireguard-config-sealed.yaml
+    kubeseal --format=yaml --cert=./.sealed-secrets.pub > apps/services/torrents/app/wireguard-config-sealed.yaml
 fi
