d280688f7f88fad5ca5b573cc21afe4ae8aeab1c
doctor_ew <doctor_ew@protonmail.com>
2024-08-10 20:11:29 -0400

    update helm upgrade script

--- scripts/helm-install-no-flux.sh
+++ scripts/helm-install-no-flux.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-PROJECT=$1
-REPO_NAME=$(cat projects/$PROJECT/app/helmrepository.yaml | yq '.metadata.name')
-REPO_URL=$(cat projects/$PROJECT/app/helmrepository.yaml | yq '.spec.url')
-NAMESPACE=$(cat projects/$PROJECT/app.yaml | yq '.metadata.namespace')
-CHART_NAME=$(cat projects/$PROJECT/app/helmrelease.yaml | yq '.spec.chart.spec.chart')
-RELEASE_NAME=$(cat projects/$PROJECT/app/helmrelease.yaml | yq '.metadata.name')
+PROJECT_PATH="$1"
+REPO_NAME=$(cat $PROJECT_PATH/app/helmrepository.yaml | yq '.metadata.name')
+REPO_URL=$(cat $PROJECT_PATH/app/helmrepository.yaml | yq '.spec.url')
+NAMESPACE=$(cat $PROJECT_PATH/app.yaml | yq '.metadata.namespace')
+CHART_NAME=$(cat $PROJECT_PATH/app/helmrelease.yaml | yq '.spec.chart.spec.chart')
+RELEASE_NAME=$(cat $PROJECT_PATH/app/helmrelease.yaml | yq '.metadata.name')
 
 echo "Project: $PROJECT"
 echo "Repository Name: $REPO_NAME"
 echo "Repository URL: $REPO_URL"
 echo "Namespace: $NAMESPACE"
 
-cat projects/$PROJECT/app/helmrelease.yaml | \
+cat $PROJECT_PATH/app/helmrelease.yaml | \
     yq '.spec.values' > /tmp/values.yaml
 
 helm repo add $REPO_NAME $REPO_URL
