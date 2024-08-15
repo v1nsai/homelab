2da0ccaa58c8dcb2995ae81c1717eda6d3675d4d
doctor_ew <doctor_ew@protonmail.com>
2024-08-15 18:07:53 -0400

    nfs config details added

---
+++ cluster/addons/nfs/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install -y nfs-kernel-server nfs-common
+sudo systemctl enable nfs-kernel-server
+
+echo "/mnt/data *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+sudo exportfs -a
+
+sudo systemctl restart nfs-kernel-server
