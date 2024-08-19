edcb8d6d693bc9a2540394e95d87237fc17c2b9c
doctor_ew <doctor_ew@protonmail.com>
2024-08-19 16:03:37 -0400

    fix typo in tls secret names

--- cluster/addons/cert-manager/install.sh
+++ cluster/addons/cert-manager/install.sh
@@ -3,17 +3,17 @@
 set -e
 
 # generate local CA key
-openssl genrsa -des3 -out tls.key 2048
-openssl req -x509 -new -nodes -key tls.key -sha256 -days 1825 -out tls.crt
+openssl genrsa -des3 -out ca.key 2048
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 1825 -out ca.crt
 
 # Trust the CA
 ## macos
-sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" tls.crt 
+sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" ca.crt 
 
 ## linux
 sudo apt-get install -y ca-certificates
-sudo cp tls.crt /usr/local/share/ca-certificates/tls.crt
+sudo cp ca.crt /usr/local/share/ca-certificates/ca.crt
 sudo update-ca-certificates
 
 # Create and seal kubernetes secret
-kubectl create secret generic tls-ca --from-file=tls.crt --from-file=tls.key -n kube-system
+kubectl create secret generic tls-ca --from-file=ca.crt --from-file=ca.key -n kube-system
