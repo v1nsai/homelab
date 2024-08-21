a135a565be818af9eec62de6d3705e9c6dbf1747
doctor_ew <doctor_ew@protonmail.com>
2024-08-21 14:24:01 -0400

    longhorn config for nfs backup target

--- cluster/addons/nfs/install.sh
+++ cluster/addons/nfs/install.sh
@@ -4,6 +4,7 @@ sudo apt install -y nfs-kernel-server nfs-common apparmor-profiles
 sudo systemctl enable nfs-kernel-server
 
 echo "/mnt/data *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/mnt/irma/longhorn *(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
 sudo exportfs -a
 
 sudo systemctl restart nfs-kernel-server
