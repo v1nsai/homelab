397e23f531391829e760acff0d6cb19f65b72f3b
doctor_ew <doctor_ew@protonmail.com>
2024-08-21 09:23:54 -0400

    minio tenant secret fixes

--- cluster/addons/minio/install.sh
+++ cluster/addons/minio/install.sh
@@ -2,10 +2,24 @@
 
 set -e
 
-kubectl create secret generic user-keys \
+# TODO put back once https://github.com/minio/operator/pull/2284 is merged
+# kubectl create secret generic user-keys \
+#     --namespace minio-tenant \
+#     --from-literal accessKey="$(openssl rand -base64 32)" \
+#     --from-literal secretKey="$(openssl rand -base64 32)" \
+#     --dry-run=client \
+#     --output yaml | \
+# kubeseal --cert ./.sealed-secrets.pub --format yaml > ./cluster/addons/minio/tenant/sealed-secrets.yaml
+
+cat <<EOF > /tmp/secret-values.yaml
+secrets:
+  name: homelab-minio-env-configuration
+  accessKey: $(openssl rand -base64 32)
+  secretKey: $(openssl rand -base64 32)
+EOF
+kubectl create secret generic secret-values \
+    --from-file=/tmp/secret-values.yaml \
     --namespace minio-tenant \
-    --from-literal accessKey="$(openssl rand -base64 32)" \
-    --from-literal secretKey="$(openssl rand -base64 32)" \
     --dry-run=client \
     --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > ./cluster/addons/minio/tenant/sealed-secrets.yaml
