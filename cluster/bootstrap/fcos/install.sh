8c45a562526924278235d7bf9dc56ec49812776f
doctor_ew <doctor_ew@protonmail.com>
2024-09-02 09:34:17 -0400

    configuring kubeadm in butane

--- cluster/bootstrap/fcos/install.sh
+++ cluster/bootstrap/fcos/install.sh
@@ -2,19 +2,23 @@
 
 set -e
 
+# Generate password hash
+cat cluster/bootstrap/fcos/password.env | docker run --interactive --rm quay.io/coreos/mkpasswd --stdin --method=yescrypt
+
 # Convert butane configs to ignition
-for FILE in cluster/bootstrap/coreos/butane/*.yaml; do
+for FILE in cluster/bootstrap/fcos/butane/*.yaml; do
   FILE=$(basename ${FILE})
-  butane cluster/bootstrap/coreos/butane/${FILE} \
+  butane cluster/bootstrap/fcos/butane/${FILE} \
     --strict \
     --pretty \
-    --output cluster/bootstrap/coreos/ignition/${FILE%.*}.json
+    --output cluster/bootstrap/fcos/ignition/${FILE%.*}.json
 done
 
 # configure FLUO for auto reboots
-kubectl apply -k https://github.com/flatcar/flatcar-linux-update-operator/tree/030e43574c229eeb5a8858f03bdcc997f38131d9/examples/deploy --dry-run=client -o yaml
+# kubectl apply -k https://github.com/flatcar/flatcar-linux-update-operator/tree/030e43574c229eeb5a8858f03bdcc997f38131d9/examples/deploy --dry-run=client -o yaml
 
 # Create installer iso
+rm -rf ~/Downloads/fcos-generic.iso
 docker run \
   --pull=always \
   --privileged \
@@ -25,9 +29,7 @@ docker run \
   --volume ./cluster/bootstrap/fcos:/data \
   --workdir /data \
   quay.io/coreos/coreos-installer:release \
-  iso customize \
-    --dest-ignition /data/ignition/generic.json \
-    --dest-console ttyS0,115200n8 \
-    --dest-console tty0 \
+  iso ignition embed \
+    --ignition-file /data/ignition/node-remote.json \
     --output /Downloads/fcos-generic.iso \
     /Downloads/fedora-coreos-40.20240808.3.0-live.x86_64.iso
