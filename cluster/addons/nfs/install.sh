94aadc6c89b9cead47fc72710f08a695dc2a18ae
doctor_ew <doctor_ew@protonmail.com>
2024-09-05 14:43:05 -0400

    fix nfs share location

--- cluster/addons/nfs/install.sh
+++ cluster/addons/nfs/install.sh
@@ -3,7 +3,7 @@
 sudo apt install -y nfs-kernel-server nfs-common apparmor-profiles
 sudo systemctl enable nfs-kernel-server
 
-echo "/mnt/data *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/mnt/irma/kubernetes *(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 echo "/mnt/irma/longhorn *(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
 sudo exportfs -a
 
