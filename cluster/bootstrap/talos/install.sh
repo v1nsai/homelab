ff45f3fe8972fa77020ae417ae31cfae656c1d9e
doctor_ew <doctor_ew@protonmail.com>
2024-09-07 10:30:08 -0400

    talos configuration updates

--- cluster/bootstrap/talos/install.sh
+++ cluster/bootstrap/talos/install.sh
@@ -1,17 +1,18 @@
 # sudo cp scripts/talosctlwrapper.sh /usr/local/bin/talosctlwrapper 
 
 # Create secrets
+VIP_ADDRESS="192.168.1.133"
 talosctl gen secrets -o cluster/bootstrap/talos/secrets.yaml.env # adding .env to make git ignore it
 talosctl gen config \
   --with-secrets cluster/bootstrap/talos/secrets.yaml.env \
   --output-types talosconfig \
   --output talosconfig \
-  talos-homelab https://192.168.1.133:6443
+  talos-homelab https://$VIP_ADDRESS:6443
 mv talosconfig ~/.talos/config # can't use ~/ in talosconfig path
 kubeseal --cert ./.sealed-secrets.pub --format yaml 
 
 # DO NOT RUN until at least one node has been booted using apply-config below
-NODE="192.168.121.89"
+NODE="192.168.1.155"
 talosctl bootstrap \
   --nodes $NODE \
   --endpoints $NODE
@@ -25,7 +26,7 @@ talosctl gen config \
   --output-types controlplane \
   --output /tmp/bigrig.yaml \
   --force \
-  talos-homelab https://192.168.1.170:6443
+  talos-homelab https://$VIP_ADDRESS:6443
 talosctl apply-config \
   --insecure \
   --nodes 192.168.1.170 \
@@ -42,7 +43,7 @@ talosctl gen config \
   --output-types controlplane \
   --output /tmp/tiffrig.yaml \
   --force \
-  talos-homelab https://192.168.1.155:6443
+  talos-homelab https://$VIP_ADDRESS:6443
 talosctl apply-config \
   --insecure \
   --nodes 192.168.1.155 \
@@ -58,7 +59,7 @@ talosctl gen config \
   --output-types controlplane \
   --output /tmp/oppenheimer.yaml \
   --force \
-  talos-homelab https://192.168.1.162:6443
+  talos-homelab https://$VIP_ADDRESS:6443
 talosctl apply-config \
   --insecure \
   --nodes 192.168.1.162 \
@@ -74,7 +75,7 @@ talosctl gen config \
   --output-types controlplane \
   --output /tmp/ASUSan.yaml \
   --force \
-  talos-homelab https://192.168.1.186:6443
+  talos-homelab https://$VIP_ADDRESS:6443
 talosctl apply-config \
   --nodes 192.168.1.186 \
   --file /tmp/ASUSan.yaml \
@@ -83,21 +84,9 @@ talosctl apply-config \
   --config-patch @cluster/bootstrap/talos/extensions/metrics-server/patch.yaml \
   --config-patch @cluster/bootstrap/talos/extensions/local-path-provisioner/patch.yaml
 
-# vm VM
-# virsh list | awk '{print $2}' | xargs -t -L1 virsh domifaddr
-VM_IPS=( 192.168.121.89 192.168.121.84 )
-for VM_IP in "${VM_IPS[@]}"; do
-  export HOSTNAME=$(openssl rand -base64 8)
-  envsubst < cluster/bootstrap/talos/install-patches/vm.yaml > /tmp/vm-patch.yaml
-  talosctl gen config \
-    --with-secrets cluster/bootstrap/talos/secrets.yaml.env \
-    --output-types controlplane \
-    --output /tmp/vm.yaml \
-    --force \
-    talos-homelab https://$VM_IP:6443
-  talosctl apply-config \
-    --insecure \
-    --nodes $VM_IP \
-    --file /tmp/vm.yaml \
-    --config-patch @/tmp/vm-patch.yaml
-done
+talosctl patch machineconfig \
+  --nodes 192.168.1.155,192.168.1.162 \
+  --endpoints 192.168.1.133 \
+  --patch-file cluster/bootstrap/talos/extensions/longhorn/patch.yaml \
+  --patch-file cluster/bootstrap/talos/extensions/metrics-server/patch.yaml \
+  --patch-file cluster/bootstrap/talos/extensions/local-path-provisioner/patch.yaml
