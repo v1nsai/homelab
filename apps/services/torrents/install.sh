c0661871402874cf61d34c947b77db56fb07b11c
doctor_ew <doctor_ew@protonmail.com>
2024-09-09 14:09:41 -0400

    wireguard troubleshooting

--- apps/services/torrents/install.sh
+++ apps/services/torrents/install.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-read -p "Recreate wireguard-config secret? [y/N]" RECREATE_SECRET
-if [ "$RECREATE_SECRET" == "y" ]; then
-    # kubectl delete secret wireguard-config \
-    #     --namespace jellyfin || true
-    kubectl create secret generic wireguard-config \
-        --namespace jellyfin \
-        --from-file=apps/services/torrents/secrets/gluetun/wg0.conf \
-        --dry-run=client \
-        -o yaml | \
-    kubeseal --format=yaml --cert=./.sealed-secrets.pub > apps/services/torrents/app/wireguard-config-sealed.yaml
-fi
+kubectl create secret generic wireguard-config \
+    --namespace jellyfin \
+    --from-file=apps/services/torrents/secrets/gluetun/wg0.conf \
+    --dry-run=client \
+    -o yaml | \
+kubeseal --format=yaml --cert=./.sealed-secrets.pub > apps/services/torrents/app/wireguard-config-sealed.yaml
