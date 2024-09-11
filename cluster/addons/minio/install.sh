62b2beb86f0ffd1dd811f44c2d8443f67c25dbea
doctor_ew <doctor_ew@protonmail.com>
2024-09-11 17:57:45 -0400

    minio tenant configuration

--- cluster/addons/minio/install.sh
+++ cluster/addons/minio/install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 set -e
+source ./cluster/addons/minio/secrets.env
 
-# TODO put back once https://github.com/minio/operator/pull/2284 is merged
-# kubectl create secret generic user-keys \
-#     --namespace minio-tenant \
-#     --from-literal accessKey="$(openssl rand -base64 32)" \
-#     --from-literal secretKey="$(openssl rand -base64 32)" \
-#     --dry-run=client \
-#     --output yaml | \
-# kubeseal --cert ./.sealed-secrets.pub --format yaml > ./cluster/addons/minio/tenant/sealed-secrets.yaml
+if [ -z "$MINIO_ROOT_USER" ] || [ -z "$MINIO_ROOT_PASSWORD" ]; then
+    echo "MINIO_ROOT_USER or MINIO_ROOT_PASSWORD is not set. Exiting."
+    exit 1
+fi
 
+# Secret values
 cat <<EOF > /tmp/secret-values.yaml
 secrets:
   name: homelab-minio-env-configuration
@@ -18,8 +16,20 @@ secrets:
   secretKey: $(openssl rand -base64 32)
 EOF
 kubectl create secret generic secret-values \
-    --from-file=/tmp/secret-values.yaml \
-    --namespace minio-tenant \
-    --dry-run=client \
-    --output yaml | \
+  --from-file=values.yaml=/tmp/secret-values.yaml \
+  --namespace minio-tenant \
+  --dry-run=client \
+  --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > ./cluster/addons/minio/tenant/sealed-secrets.yaml
+
+# Minio config
+cat <<EOF > /tmp/config.env
+export MINIO_ROOT_USER="$MINIO_ROOT_USER"
+export MINIO_ROOT_PASSWORD="$MINIO_ROOT_PASSWORD"
+EOF
+kubectl create secret generic minio-tenant-env \
+  --from-file=config.env=/tmp/config.env \
+  --namespace minio-tenant \
+  --dry-run=client \
+  --output yaml | \
+kubeseal --cert ./.sealed-secrets.pub --format yaml | tee -a ./cluster/addons/minio/tenant/sealed-secrets.yaml
