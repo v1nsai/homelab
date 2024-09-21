a76d98415755f471dacd2510f30025512b89bb78
doctor_ew <doctor_ew@protonmail.com>
2024-09-21 10:43:08 -0400

    minio wrapper script

--- scripts/minio-aws-cli-wrapper.sh
+++ scripts/minio-aws-cli-wrapper.sh
@@ -3,5 +3,7 @@
 # sudo cp scripts/minio-aws-cli-wrapper.sh /usr/local/bin/minio-aws-cli
 set -e
 
-export AWS_CA_BUNDLE="/Users/doctor_ew/code/homelab/apps/services/minio/secrets/ca.crt"
-aws --endpoint-url https://minio-api.internal/ "$@"
+aws \
+    --ca-bundle /Users/doctor_ew/code/homelab/apps/services/minio/secrets/ca.crt \
+    --endpoint-url https://minio-api.internal/ \
+    "$@"
