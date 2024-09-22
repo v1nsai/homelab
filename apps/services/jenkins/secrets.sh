44feb9b4e56fd26c23039ab7ecc5eb63929174b7
doctor_ew <doctor_ew@protonmail.com>
2024-09-22 15:38:58 -0400

    switch to official jenkins helm chart

--- apps/services/jenkins/secrets.sh
+++ apps/services/jenkins/secrets.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-cat <<EOF > /tmp/secret-values.yaml
-jenkinsUser: doctor_ew
-jenkinsPassword: $(openssl rand -base64 20)
-EOF
+# bitnami secrets
+# cat <<EOF > /tmp/secret-values.yaml
+# jenkinsUser: doctor_ew
+# jenkinsPassword: $(openssl rand -base64 20)
+# EOF
 
-kubectl create secret generic jenkins-secret \
+# kubectl create secret generic jenkins-secret \
+#     --namespace jenkins \
+#     --from-file=/tmp/secret-values.yaml \
+#     --dry-run=client \
+#     --output yaml | \
+# kubeseal --cert ./.sealed-secrets.pub --format yaml > ./apps/services/jenkins/app/sealed-secrets.yaml
+
+cat <<EOF > /tmp/secret-values.yaml
+controller:
+    admin:
+        username: doctor_ew
+        password: $(openssl rand -base64 20)
+EOF
+kubectl create secret generic secret-values \
     --namespace jenkins \
     --from-file=/tmp/secret-values.yaml \
     --dry-run=client \
