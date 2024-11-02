785268c00f26ade184dfb89258ab31064f1e8036
doctor_ew <doctor_ew@protonmail.com>
2024-11-02 18:59:20 -0400

    remove plex from flux due to issues with plex claim

--- apps/services/plex/install.sh
+++ apps/services/plex/install.sh
@@ -16,3 +16,20 @@ kubectl create secret generic secret-values \
     --dry-run=client \
     --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > ./apps/services/plex/app/sealed-secrets.yaml
+
+# install no flux
+kubectl create namespace plex
+kubectl label namespace plex \
+  pod-security.kubernetes.io/enforce=privileged \
+  pod-security.kubernetes.io/enforce-version=latest \
+  pod-security.kubernetes.io/warn=privileged \
+  pod-security.kubernetes.io/warn-version=latest \
+  pod-security.kubernetes.io/audit=privileged \
+  pod-security.kubernetes.io/audit-version=latest
+
+source apps/services/plex/.env
+helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
+helm upgrade --install plex plex/plex-media-server \
+  --namespace plex \
+  --set extraEnv.PLEX_CLAIM=$PLEX_CLAIM \
+  --values apps/services/plex/values.yaml
