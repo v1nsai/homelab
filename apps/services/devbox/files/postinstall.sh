3d70f7cb893f852a170ee7e5409bca901461bacb
doctor_ew <doctor_ew@protonmail.com>
2024-12-08 13:00:49 -0500

    last postinstall testing

--- apps/services/devbox/files/postinstall.sh
+++ apps/services/devbox/files/postinstall.sh
@@ -44,7 +44,12 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 
 echo "Installing Node..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-source ~/.bashrc
+cat <<EOF >> /home/$USERNAME/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
+source /home/$USERNAME/.bashrc
 nvm install 18
 nvm use 18
 
