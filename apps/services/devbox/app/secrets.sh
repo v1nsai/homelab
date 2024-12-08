8015cdf45e2f2402d256e9f14e3c611a5977af02
doctor_ew <doctor_ew@protonmail.com>
2024-12-08 10:54:47 -0500

    fix sealed-secret namespace

--- apps/services/devbox/app/secrets.sh
+++ apps/services/devbox/app/secrets.sh
@@ -4,6 +4,7 @@ set -e
 source apps/services/devbox/.env
 
 kubectl create secret generic ssh-pubkey \
+    --namespace=devbox \
     --from-literal=authorized_keys="$SSH_PUBKEY" \
     --dry-run=client -o yaml | \
 kubeseal --format=yaml --cert=./.sealed-secrets.pub > ./apps/services/devbox/app/sealed-secrets.yaml
