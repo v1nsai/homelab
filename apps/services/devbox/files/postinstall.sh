c6972dbb8e5a786d5ee76036926d296e715c91d2
doctor_ew <doctor_ew@protonmail.com>
2024-12-08 12:00:08 -0500

    switch back to ubuntu base and install docker

--- apps/services/devbox/files/postinstall.sh
+++ apps/services/devbox/files/postinstall.sh
@@ -8,7 +8,7 @@ if [ -f /postinstall-has-run ]; then
 fi
 
 apt update
-apt install -y openssh-server vim unminimize sudo nnn git
+apt install -y openssh-server vim unminimize sudo nnn git wget
 unminimize
 
 # ssh
@@ -23,9 +23,25 @@ cp /authorized_keys /home/$USERNAME/.ssh/authorized_keys
 chown $USERNAME:$USERNAME /home/$USERNAME/.ssh/authorized_keys
 chmod 600 /home/$USERNAME/.ssh/authorized_keys
 
-# node 
+# docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update 
 
+# Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+nvm install 18
+nvm use 18
 
 # If everything runs successfully, don't bother running next startup
 touch /postinstall-has-run
