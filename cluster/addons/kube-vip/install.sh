25858ac6f6077a68a9e380ad1a8016363b9b4019
doctor_ew <doctor_ew@protonmail.com>
2025-02-16 19:29:04 -0500

    replace metallb with kube-vip

---
+++ cluster/addons/kube-vip/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo "Creating RBAC settings..."
+kubectl apply -f https://kube-vip.io/manifests/rbac.yaml
+
+echo "Generating a manifest..."
+KVVERSION=$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name")
+# alias kube-vip="ctr image pull ghcr.io/kube-vip/kube-vip:$KVVERSION; ctr run --rm --net-host ghcr.io/kube-vip/kube-vip:$KVVERSION vip /kube-vip"
+alias kube-vip="docker run --network host --rm ghcr.io/kube-vip/kube-vip:$KVVERSION"
+
+kube-vip manifest daemonset \
+    --inCluster \
+    --taint \
+    --services \
+    --arp \
+    --leaderElection
