370d3cc4a1d4ce958a97a4861937a402d9466a72
doctor_ew <doctor_ew@protonmail.com>
2025-02-17 20:43:01 -0500

    update plex install script

--- apps/services/plex/install.sh
+++ apps/services/plex/install.sh
@@ -6,19 +6,8 @@ set -e
 echo "Please enter your Plex Claim token:"
 read -r PLEX_CLAIM
 
-cat <<EOF > /tmp/secret-values.yaml
-extraEnv:
-  PLEX_CLAIM: $PLEX_CLAIM
-EOF
-kubectl create secret generic secret-values \
-    --namespace=plex \
-    --from-file=/tmp/secret-values.yaml \
-    --dry-run=client \
-    --output yaml | \
-kubeseal --cert ./.sealed-secrets.pub --format yaml > ./apps/services/plex/app/sealed-secrets.yaml
-
-# install no flux
-kubectl create namespace plex
+# install without flux (easier to deal with the claim token)
+kubectl create namespace plex || true
 kubectl label namespace plex \
   pod-security.kubernetes.io/enforce=privileged \
   pod-security.kubernetes.io/enforce-version=latest \
@@ -27,7 +16,6 @@ kubectl label namespace plex \
   pod-security.kubernetes.io/audit=privileged \
   pod-security.kubernetes.io/audit-version=latest
 
-source apps/services/plex/.env
 helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
 helm upgrade --install plex plex/plex-media-server \
   --namespace plex \
