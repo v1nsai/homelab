a77b9e9cb68fd50b020979deab3c23e3b68e003c
doctor_ew <doctor_ew@protonmail.com>
2025-02-17 20:42:07 -0500

    cert-manager script update

--- cluster/addons/cert-manager/install.sh
+++ cluster/addons/cert-manager/install.sh
@@ -8,7 +8,7 @@ openssl req -x509 -new -nodes -key ca.key -sha256 -days 1825 -out ca.crt
 
 # Trust the CA
 ## macos
-sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" ca.crt 
+sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" cluster/addons/cert-manager/ca.crt
 
 ## linux
 sudo apt-get install -y ca-certificates
