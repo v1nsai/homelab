400ca836b5ab3af594d9b1950a06b06a45338229
doctor_ew <doctor_ew@protonmail.com>
2025-06-01 17:51:13 -0400

    plex install script updates

--- apps/services/plex/install.sh
+++ apps/services/plex/install.sh
@@ -5,18 +5,21 @@ set -e
 
 echo "Please enter your Plex Claim token:"
 read -r PLEX_CLAIM
+echo "Using Plex Claim token: $PLEX_CLAIM"
+export PLEX_CLAIM
 
 # install without flux (easier to deal with the claim token)
-kubectl create namespace plex || true
-kubectl label namespace plex \
-  pod-security.kubernetes.io/enforce=privileged \
-  pod-security.kubernetes.io/enforce-version=latest \
-  pod-security.kubernetes.io/warn=privileged \
-  pod-security.kubernetes.io/warn-version=latest \
-  pod-security.kubernetes.io/audit=privileged \
-  pod-security.kubernetes.io/audit-version=latest
+# kubectl create namespace plex || true
+# kubectl label namespace plex \
+#   pod-security.kubernetes.io/enforce=privileged \
+#   pod-security.kubernetes.io/enforce-version=latest \
+#   pod-security.kubernetes.io/warn=privileged \
+#   pod-security.kubernetes.io/warn-version=latest \
+#   pod-security.kubernetes.io/audit=privileged \
+#   pod-security.kubernetes.io/audit-version=latest
 
-helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
+# helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
+helm delete -n plex plex || true
 helm upgrade --install plex plex/plex-media-server \
   --namespace plex \
   --values apps/services/plex/values.yaml \
