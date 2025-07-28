d8076e232f638352c8a8a0bf57d732c1b21e1339
Dr Ew <jamzsabb@gmail.com>
2025-07-27 20:10:19 -0400

    force nvidia on gpu enabled nodes for plex

--- apps/services/plex/install.sh
+++ apps/services/plex/install.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
 set -e
-# source projects/plex/.env
+source apps/services/plex/.env
 
 echo "Please enter your Plex Claim token:"
 read -r PLEX_CLAIM
 echo "Using Plex Claim token: $PLEX_CLAIM"
 export PLEX_CLAIM
-
-# install without flux (easier to deal with the claim token)
-# kubectl create namespace plex || true
-# kubectl label namespace plex \
-#   pod-security.kubernetes.io/enforce=privileged \
-#   pod-security.kubernetes.io/enforce-version=latest \
-#   pod-security.kubernetes.io/warn=privileged \
-#   pod-security.kubernetes.io/warn-version=latest \
-#   pod-security.kubernetes.io/audit=privileged \
-#   pod-security.kubernetes.io/audit-version=latest
-
-# helm repo add plex https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages
-helm delete -n plex plex || true
-helm upgrade --install plex plex/plex-media-server \
-  --namespace plex \
-  --values apps/services/plex/values.yaml \
-  --set extraEnv.PLEX_CLAIM=$PLEX_CLAIM
-
-kubectl apply -f ./apps/services/plex/ingress.yaml
 
 # generate the plex claim secret
 kubectl create secret generic plex-claim \
