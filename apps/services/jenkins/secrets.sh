a6848defd28ec9d0f358fa27bf37d478e58fec1e
doctor_ew <doctor_ew@protonmail.com>
2025-08-17 11:35:04 -0400

    jenkins harbor secret

--- apps/services/jenkins/secrets.sh
+++ apps/services/jenkins/secrets.sh
@@ -26,3 +26,26 @@ kubectl create secret generic secret-values \
     --dry-run=client \
     --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > ./apps/services/jenkins/app/sealed-secrets.yaml
+
+# Harbor secret
+source apps/services/jenkins/.env
+cat <<EOF > /tmp/harbor-secret.json
+{
+    "auths": {
+        "harbor.internal": {
+        "username": "robot\$jenkins",
+        "password": "${HARBOR_PASSWORD}"
+        }
+    }
+}
+EOF
+kubectl create secret generic harbor-docker-config \
+    --namespace jenkins \
+    --from-file=config.json=/tmp/harbor-secret.json \
+    --dry-run=client \
+    --output yaml | \
+kubeseal --cert ./.sealed-secrets.pub --format yaml | tee -a ./apps/services/jenkins/app/sealed-secrets.yaml
+
+# kubectl -n jenkins delete secret harbor-docker-config || true
+# kubectl -n jenkins create secret generic harbor-docker-config \
+#   --from-file=config.json=/tmp/dockercfg/config.json
