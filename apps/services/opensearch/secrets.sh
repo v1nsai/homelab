bfdf87e5e6be7336fe546add97ba9dd3e78c28e1
doctor_ew <doctor_ew@protonmail.com>
2026-01-07 19:56:10 -0500

    opensearch auth

--- apps/services/opensearch/secrets.sh
+++ apps/services/opensearch/secrets.sh
@@ -4,14 +4,14 @@ set -e
 source apps/services/opensearch/.env
 
 echo "Configuring secret values..."
-cat << EOF > /tmp/secret-values.yaml
+cat << EOF > /tmp/data
 username: "$USERNAME"
 password: "$PASSWORD"
 EOF
 kubectl create secret generic -n opensearch opensearch-auth \
-    --from-file=/tmp/secret-values.yaml \
+    --from-file=/tmp/data \
     --dry-run=client \
     --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml | tee -a apps/services/opensearch/opensearch/opensearch-auth-sealed.yaml
 
-rm -rf /tmp/secret-values.yaml
+rm -rf /tmp/data
