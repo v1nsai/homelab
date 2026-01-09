aec1c8f217b17cfd8e7bf4062d455f5de5f6d7d7
doctor_ew <doctor_ew@protonmail.com>
2026-01-09 15:22:56 -0500

    troubleshooting secret injection on this stupid helm chart

--- apps/services/opensearch/secrets.sh
+++ apps/services/opensearch/secrets.sh
@@ -5,8 +5,7 @@ source apps/services/opensearch/.env
 
 echo "Configuring secret values..."
 kubectl create secret generic -n opensearch opensearch-auth \
-    --from-literal=username="$USERNAME" \
-    --from-literal=password="$PASSWORD" \
+    --from-literal=OPENSEARCH_INITIAL_ADMIN_PASSWORD="$PASSWORD" \
     --dry-run=client \
     --output yaml | \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > apps/services/opensearch/opensearch/opensearch-auth-sealed.yaml
