7b1e06ed447d9ceeaf01ebd25310c9935c507d7c
doctor_ew <doctor_ew@protonmail.com>
2026-01-10 12:31:10 -0500

    move opensearch to data-science instead of services

--- apps/data-science/opensearch/secrets.sh
+++ apps/data-science/opensearch/secrets.sh
@@ -11,3 +11,12 @@ kubectl create secret generic -n opensearch opensearch-auth \
 kubeseal --cert ./.sealed-secrets.pub --format yaml > apps/services/opensearch/opensearch/opensearch-auth-sealed.yaml
 
 rm -rf /tmp/data
+
+# Update service name after installation because its the exact same in opensearch charts for some reason
+kubectl -n opensearch patch svc opensearch-cluster-master --type='json' -p='[
+    {
+        "op": "replace",
+        "path": "/metadata/name",
+        "value": "opensearch"
+    }
+]'
