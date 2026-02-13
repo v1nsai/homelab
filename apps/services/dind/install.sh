822bddd7b4f7beb2376a4cba6223fc972efb80cc
doctor_ew <doctor_ew@protonmail.com>
2026-02-13 13:22:02 -0500

    docker install script

---
+++ apps/services/dind/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# pull certs from k8s secrets
+kubectl get secret dind-client-tls \
+    --namespace dind \
+    --output jsonpath='{.data.tls\.crt}' \
+    | base64 --decode > $HOME/.docker/talos-homelab/cert.pem
+kubectl get secret dind-client-tls \
+    --namespace dind \
+    --output jsonpath='{.data.tls\.key}' \
+    | base64 --decode > $HOME/.docker/talos-homelab/key.pem
+kubectl get secret dind-client-tls \
+    --namespace dind \
+    --output jsonpath='{.data.ca\.crt}' \
+    | base64 --decode > $HOME/.docker/talos-homelab/ca.pem
+
+# create docker context
+docker context create talos-homelab \
+  --description "Remote docker daemon on Talos homelab" \
+  --docker "host=tcp://dind.internal:2376,\
+            ca=$HOME/.docker/talos-homelab/ca.pem,\
+            cert=$HOME/.docker/talos-homelab/cert.pem,\
+            key=$HOME/.docker/talos-homelab/key.pem"
